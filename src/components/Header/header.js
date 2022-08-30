62b80d2809455674839c026e28851c71aff5c121
sainath-stack <123sainathreddy100@gmail.com>
2022-08-31 00:11:26 +0530

    latest Changes

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,25 +1,36 @@
 import React from 'react'
+import editIcon from '../../assets/svg/edit.svg'
 import styles from './header.module.scss'
 export default function Header() {
     return (
-       <div className='card'>
-         <div className='container-fluid container1'>
-            <div className='row'>
-                <h1 className={`${styles['title']} pt-3 pb-0`}>Meter Headend Systems</h1>
-            </div>
-            <hr style={{height:'0px',color:'grey',backgroundColor:"gray"}}/>
-            <div className='row'>
-                <div className='col'>
-    
-                </div>
-                <div className='col'>
+        <div className='container-fluid'>
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class={`${styles['card-header']} my-0 py-2`}>
+                            <h4 class={`${styles['title']} ps-3`}>Meter Headend Systems</h4>
+                        </div>
 
-                </div>
-                <div className='col'>
-
+                        <div class={`${styles['card-footer']}`}>
+                            <div class="row">
+                                <div class="col d-sm-flex justify-content-between">
+                                    <div class="w-auto">
+                                        <button class="btn btn-outline-primary me-1 mb-1">Cancel</button>
+                                    </div>
+                                    <div class="input-group" style={{width:"60%"}}>
+                                        <input type="text" class={`${styles['form-control']} form-control border-0 border-bottom border-2 w-auto` } placeholder="Your Adapter Name" /><button class="input-group-text bg-transparent border-0 border-bottom border-2">
+                                          <img src={editIcon}/>
+                                        </button>
+                                    </div>
+                                    <div class="w-auto">
+                                        <button class="btn btn-outline-primary float-end">Save</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-       </div>
     )
 }
--- src/pages/configureAdapters/meterHeadendSystems/index.js
+++ src/pages/configureAdapters/meterHeadendSystems/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../../../components/Header/header'
 export default function MeterHeadendSystems(){
     return(
-        <div className='ms-5 me-5 mt-4'>
+        <div className='mt-4'>
             <Header />
         </div>
     )
